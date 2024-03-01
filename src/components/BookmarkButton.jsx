46e931a532ae5b816e04815692a99ef44d8398e3
seokhyun Yoon <gah1234@naver.com>
2024-03-01 19:37:08 +0900

    test2 https mixed content error

--- src/components/BookmarkButton.jsx
+++ src/components/BookmarkButton.jsx
@@ -56,7 +56,7 @@ function BookmarkButton() {
       console.log(json.message);
       handleFavoriteButton();
     } catch (err) {
-      console.log("실패했다" + err);
+      console.log("실패했다1" + err);
     }
   };
 
